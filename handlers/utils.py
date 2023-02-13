9c0f46dfa4f10d33b73abf50436d530c1232f786
Andrew <dushezbaranov@gmail.com>
2023-02-13 14:31:51 +0300

    Fixed typo in the last activity column name

--- handlers/utils.py
+++ handlers/utils.py
@@ -4,5 +4,5 @@ from pytz import timezone
 
 
 def track_user_activity(user_id, table, button):
-    db.update_user(table, "last_activity_time", user_id, f"{datetime.now(timezone(tz)).strftime('%d.%m.%Y %H:%M:%S')}, "
-                                                         f"кнопка {button}")
+    db.update_user(table, "last_activity", user_id, f"{datetime.now(timezone(tz)).strftime('%d.%m.%Y %H:%M:%S')}, "
+                                                    f"кнопка {button}")
