27814299ad7c9b961e2ce21d581e195161e7b8ac
Andrew <dushezbaranov@gmail.com>
2023-07-09 15:45:08 +0300

    Remove tracking of user activity through a function

--- handlers/utils.py
+++
@@ -1,8 +0,0 @@
-from datetime import datetime
-from loader import db, tz
-from pytz import timezone
-
-
-def track_user_activity(user_id, table, button):
-    db.update_user(table, "last_activity", user_id, f"{datetime.now(timezone(tz)).strftime('%d.%m.%Y %H:%M:%S')}, "
-                                                    f"кнопка {button}")
