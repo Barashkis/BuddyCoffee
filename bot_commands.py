cee563f99a706de7465c32d2ef11ac79c8177a82
Andrew <dushezbaranov@gmail.com>
2023-07-19 21:23:15 +0300

    set_bot_commands refactoring

--- bot_commands.py
+++ bot_commands.py
@@ -2,6 +2,9 @@ from aiogram import types
 
 
 async def set_default_commands(dp):
-    await dp.bot.set_my_commands([
-        types.BotCommand("menu", 'Главное меню')
-    ])
+    await dp.bot.set_my_commands(
+        [
+            types.BotCommand('start', 'Перезапустить бота'),
+            types.BotCommand('menu', 'Главное меню'),
+        ]
+    )
